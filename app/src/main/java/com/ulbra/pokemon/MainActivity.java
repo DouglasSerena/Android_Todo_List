c7c9184df4f40b2f7218bc63bc301d90c1c94397
DouglasSerena <douglas222serena@rede.ulbra.br>
2021-06-16 19:33:00 -0300

    first commit

---
+++ app/src/main/java/com/ulbra/pokemon/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ulbra.pokemon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+}
